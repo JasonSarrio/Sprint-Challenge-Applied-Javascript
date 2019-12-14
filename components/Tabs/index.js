84290c1e5477a7fe5b153f3744fc5007717de1c0
JasonSarrio <jsarrio16@gmail.com>
2019-12-13 18:18:31 -0600

    Header Index JS Finish MVP on Mark

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsDiv = document.querySelector(".topics");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    response.data.topics.forEach(topic => {
+      topicsDiv.appendChild(tabs(topic));
+    });
+  });
+
+//
+//  The tab component should look like this:
+//    <div class="tab">topic here</div>
+
+function tabs(x) {
+  const newTab = document.createElement("div");
+  newTab.classList.add("tab");
+  newTab.textContent = x;
+
+  return newTab;
+}
